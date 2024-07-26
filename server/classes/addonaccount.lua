3f9ce9ef2d475d7735293ebfe92ceefce8341143
Mirow <maxim.doerr@icloud.com>
2024-07-26 21:03:06 +0200

    feat(server.lua): added exports to get addon accounts

--- server/classes/addonaccount.lua
+++ server/classes/addonaccount.lua
@@ -5,29 +5,30 @@ function CreateAddonAccount(name, owner, money)
 	self.owner = owner
 	self.money = money
 
-	function self.addMoney(m)
-		self.money = self.money + m
+	function self.addMoney(amount)
+		self.money = self.money + amount
 		self.save()
-		TriggerEvent('esx_addonaccount:addMoney', self.name, m)
+		TriggerEvent('esx_addonaccount:addMoney', self.name, amount)
 	end
 
-	function self.removeMoney(m)
-		self.money = self.money - m
+	function self.removeMoney(amount)
+		self.money = self.money - amount
 		self.save()
-		TriggerEvent('esx_addonaccount:removeMoney', self.name, m)
+		TriggerEvent('esx_addonaccount:removeMoney', self.name, amount)
 	end
 
-	function self.setMoney(m)
-		self.money = m
+	function self.setMoney(amount)
+		self.money = amount
 		self.save()
-		TriggerEvent('esx_addonaccount:setMoney', self.name, m)
+		TriggerEvent('esx_addonaccount:setMoney', self.name, amount)
 	end
 
 	function self.save()
 		if self.owner == nil then
-			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ?', {self.money, self.name})
+			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ?', { self.money, self.name })
 		else
-			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ? AND owner = ?', {self.money, self.name, self.owner})
+			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ? AND owner = ?',
+				{ self.money, self.name, self.owner })
 		end
 		TriggerClientEvent('esx_addonaccount:setMoney', -1, self.name, self.money)
 	end
